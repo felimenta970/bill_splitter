4be8b51a3e831ae007ed9e495cdebd31d3193d54
Felipe Pimenta <felimenta97@gmail.com>
2020-08-27 14:43:54 -0300

    Initial commit

---
+++ js/javaScript.js
@@ -0,0 +1,98 @@
+let numPessoas = 1;
+let numItens = 1;
+let pessoas = [];
+let itens = [];
+
+function adicionaPessoa() {
+
+
+    let pessoa = {
+        nome: document.getElementById('nome' + numPessoas).value,
+        conta: 0,
+    };
+
+    pessoas.push(pessoa);
+
+    listaNomes();
+
+    numPessoas++;
+
+    console.log(pessoas);
+    console.log(numPessoas);
+
+}
+
+function novaPessoa () {
+    let divGeral = document.querySelector('#novasPessoas');
+    let input = document.createElement('input');
+
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'nome' + numPessoas);
+
+    let button = document.createElement('button');
+
+    button.setAttribute('id', 'novaPessoa');
+    button.setAttribute('onclick', 'adicionaPessoa()');
+    let br = document.createElement('br');
+    button.innerHTML = "Adicionar";
+
+    divGeral.appendChild(input);
+    divGeral.appendChild(button);
+    divGeral.appendChild(br);
+}
+
+function listaNomes() {
+    let div = document.getElementById('nomesParaItens');
+    let aux = numPessoas - 1;
+    br = document.createElement('br');
+
+    let checkbox = document.createElement('input');
+
+    checkbox.type = 'checkbox';
+    checkbox.name = 'nome' + aux;
+    checkbox.value = pessoas[numPessoas-1].nome;
+    checkbox.id = 'nome' + aux;
+
+    let label = document.createElement('label');
+    label.htmlFor = 'nome' + aux;
+    label.appendChild(document.createTextNode(pessoas[numPessoas-1].nome));
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    div.appendChild(br);
+}
+
+function adicionarItem() {
+    
+    let cont = 0;
+
+    let item = {
+        nome: document.getElementById('item' + numItens).value,
+        valor: document.getElementById('preco' + numItens).value
+    }
+
+    let nomes = [];
+
+    itens.push(item);
+
+
+    $('input[type=checkbox]').each(function() {
+        if (this.checked) {
+            cont++;
+            nomes.push(this.value);
+        }
+    });
+
+    let valorInd = item.valor/cont;
+
+    for (let i = 0; i < nomes.length; i++) {
+        for (let j = 0; j < pessoas.length; j++) {
+            if (nomes[i] === pessoas[j].nome) {
+                pessoas[j].conta += valorInd;
+            }
+        }
+    }
+}
+
+function divisorConta() {
+
+}
