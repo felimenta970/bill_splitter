bc0cb427968a384da6181ab83b7aa195b468af23
Felipe Pimenta <felimenta97@gmail.com>
2020-08-27 21:06:53 -0300

    Finalização das funções e estilos

--- js/javaScript.js
+++ js/javaScript.js
@@ -5,70 +5,136 @@ let itens = [];
 
 function adicionaPessoa() {
 
+    if(document.getElementById('nome').value == '') {
+        $('#avisoPessoa').empty();
 
-    let pessoa = {
-        nome: document.getElementById('nome').value,
-        conta: 0,
-    };
-
-    pessoas.push(pessoa);
-
-    listaNomesDiv();
-    listaNomesGorjeta();
-
-    numPessoas++;
-
-    $('#sucessoPessoa').empty();
-
-    let div = document.getElementById('sucessoPessoa');
-    let aviso = document.createElement('p');
+        let div = document.getElementById('avisoPessoa');
+        div.setAttribute('class', 'aviso');
+        let aviso = document.createElement('p');
     
-    aviso.innerHTML = pessoa.nome + " adicionado com sucesso!";
+        aviso.innerHTML = "Nome vazio";
 
-    div.appendChild(aviso);
+        div.appendChild(aviso);
 
+    } else {
+
+        let pessoa = {
+            nome: document.getElementById('nome').value,
+            conta: 0,
+        };
+    
+        pessoas.push(pessoa);
+    
+        listaNomesDiv();
+        listaNomesGorjeta();
+    
+        numPessoas++;
+    
+        $('#avisoPessoa').empty();
+    
+        let div = document.getElementById('avisoPessoa');
+        div.setAttribute('class', 'sucesso');
+        let aviso = document.createElement('p');
+        
+        aviso.innerHTML = pessoa.nome + " adicionado com sucesso!";
+    
+        div.appendChild(aviso);
+    }
 
 }
 
 function adicionarItem() {
+
+    if (document.getElementById('item').value == ''){
+
+        $('#avisoItem').empty();
+
+        let div = document.getElementById('avisoItem');
+        div.setAttribute('class', 'aviso');
+        let aviso = document.createElement('p');
     
-    let cont = 0;
+        aviso.innerHTML = "Item vazio";
 
-    let item = {
-        nome: document.getElementById('item').value,
-        valor: document.getElementById('preco').value
-    }
+        div.appendChild(aviso);
 
-    let nomes = [];
+    } else {
+        if (document.getElementById('preco').value == '') {
 
-    itens.push(item);
+            $('#avisoItem').empty();
 
+            let div = document.getElementById('avisoItem');
+            div.setAttribute('class', 'aviso');
+            let aviso = document.createElement('p');
+    
+            aviso.innerHTML = "Consumo sem preço";
 
-    $('input[type=checkbox').each(function() {
-        if (this.checked) {
-            cont++;
-            nomes.push(this.value);
-        }
-    });
+            div.appendChild(aviso);
 
-    let valorInd = item.valor/cont;
+        } else {
 
-    for (let i = 0; i < nomes.length; i++) {
-        for (let j = 0; j < pessoas.length; j++) {
-            if (nomes[i] === pessoas[j].nome) {
-                pessoas[j].conta += valorInd;
+            let cont = 0;
+
+            $('.nomesItens').each(function() {
+                if(this.checked) {
+                    cont++;
+                    console.log(cont);
+                }
+            });
+
+            if (cont == 0) {
+                console.log('aqui');
+                $('#avisoItem').empty();
+
+                let div = document.getElementById('avisoItem');
+                div.setAttribute('class', 'aviso');
+                let aviso = document.createElement('p');
+    
+                aviso.innerHTML = "Ninguem vai pagar?";
+
+                div.appendChild(aviso);
+
+            } else {
+
+                let item = {
+                    nome: document.getElementById('item').value,
+                    valor: document.getElementById('preco').value
+                }
+        
+                let nomes = [];
+    
+                itens.push(item);
+    
+    
+                $('input[type=checkbox]').each(function() {
+                    if (this.checked) {
+                        nomes.push(this.value);
+                    }
+                });
+    
+                let valorInd = item.valor/cont;
+    
+                for (let i = 0; i < nomes.length; i++) {
+                    for (let j = 0; j < pessoas.length; j++) {
+                        if (nomes[i] === pessoas[j].nome) {
+                            pessoas[j].conta += valorInd;
+                        }
+                    }
+                }
+    
+                $('#avisoItem').empty();
+    
+                let div = document.getElementById('avisoItem');
+                div.setAttribute('class', 'sucesso');
+                let aviso = document.createElement('p');
+        
+                aviso.innerHTML = item.nome + " adicionado com sucesso!";
+    
+                div.appendChild(aviso);
             }
+
         }
     }
-
-    $('#sucessoItem').empty();
-
-    let div = document.getElementById('sucessoItem');
-    let aviso = document.createElement('p');
     
-    aviso.innerHTML = item.nome + " adicionado com sucesso!";
-
-    div.appendChild(aviso);
 }
 
 function listaNomesDiv() {
@@ -82,7 +148,7 @@ function listaNomesDiv() {
     checkbox.type = 'checkbox';
     checkbox.name = 'nome' + aux;
     checkbox.value = pessoas[numPessoas-1].nome;
-    checkbox.id = 'nomesItens';
+    checkbox.setAttribute('class', 'nomesItens');
 
     let label = document.createElement('label');
     label.htmlFor = 'nome' + aux;
@@ -122,11 +188,12 @@ function limpaCampoItem() {
     document.getElementById('item').value = '';
     document.getElementById('preco').value = '';
 
-    $('#nomesItens').each(function() {
+    $('.nomesItens').each(function() {
         if(this.checked) {
             this.checked = false;
         }
     });
+
 }
 
 function adicionaGorjeta() {
@@ -149,14 +216,35 @@ function adicionaGorjeta() {
             }
         }
     }
-
-    console.log(nomes);
-
-    console.log(pessoas);
+    
 }
 
 function divisorConta() {
     adicionaGorjeta();
 
+    let pessoaC;
+    let valor;
 
+    for (let i = 0; i < pessoas.length; i++) {
+
+        let div = document.getElementById('resultados');
+
+        let nome = document.createElement('div');
+        nome.setAttribute('class', 'nomeConta');
+
+        let conta = document.createElement('div');
+        conta.setAttribute('class', 'valorConta');
+        
+        let br = document.createElement('br');
+
+        pessoaC = pessoas[i].nome;
+        valor = pessoas[i].conta;
+
+        nome.innerHTML = pessoaC;
+        conta.innerHTML = ': R$ ' + valor.toFixed(2);
+
+        div.appendChild(nome);
+        div.appendChild(conta);
+        div.appendChild(br);
+    }
 }
